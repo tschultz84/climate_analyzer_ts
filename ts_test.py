6de6a5e2a635c240caaff6a9005849e0ccf9cdf8
alyosha_84 <77863290+tschultz84@users.noreply.github.com>
2022-01-14 20:46:40 -0700

    1/14/22 done

--- ts_test.py
+++ ts_test.py
@@ -4,49 +4,50 @@ Created on Wed Dec 29 13:10:53 2021
 
 @author: 14154
 """
-
+import Station_Loader_ts as load
+import Station_analyzer_ts as analyze
 
 
 #%%
-bzdata=LoadStation([45.647256643331126,-111.04060494981753],10,True,True) #Loading in Bozeman, MT coordinates
+bzdata=load.LoadStation([45.647256643331126,-111.04060494981753],10,True,True) #Loading in Bozeman, MT coordinates
 
 #%%
 
 date1 = '2021-6-1'
 date2 = '2021-8-30'
-bzcalc=StationAnalyzer(bzdata.station_data_clean,date1,date2,display=True)
+bzcalc=analyze.StationAnalyzer(bzdata.station_data_clean,date1,date2,display=True)
 
 #test=bzcalc.tmid_selection(bzcalc.refperiod)
 #print(test)
 
 
 #%%
-obdata=LoadStation([37.755663644,-122.506497974],10,True,True) #Loading in Ocean Beach, SF coordinates
+obdata=load.LoadStation([37.755663644,-122.506497974],10,True,True) #Loading in Ocean Beach, SF coordinates
 #obdata.calculate_tmid_new(obdata.all_data_np)
 
 
 #%%
 date1 = '2010-1-1'
 date2 = '2020-12-1'
-obcalc=StationAnalyzer(obdata.station_data_clean,date1,date2,display=True)
+obcalc=analyze.StationAnalyzer(obdata.station_data_clean,date1,date2,display=True)
 
 #print(obcalc.kpi)
 #obcalc.key_charts()
 
 #%%
-sddata=LoadStation([32.741947,-117.239571],10,True,True) #Loading in Ocean Beach, San Diego coordinates
+sddata=load.LoadStation([32.741947,-117.239571],10,True,True) #Loading in Ocean Beach, San Diego coordinates
 #%%
 date1 = '2000-1-1'
 date2 = '2020-12-31'
-sdcalc=StationAnalyzer(sddata.station_data_clean)
+sdcalc=analyze.StationAnalyzer(sddata.station_data_clean)
 
 #%%
 
-dvtdata=LoadStation([32.659167, -116.099167],10,True,True) #Loading in Desert View Tower data
+dvtdata=load.LoadStation([32.659167, -116.099167],10,True,True) #Loading in Desert View Tower data
 #%%
 date1 = '2000-1-1'
 date2 = '2020-12-31'
-dvtcalc=StationAnalyzer(dvtdata.station_data_clean,date1,date2,display=True)
+dvtcalc=analyze.StationAnalyzer(dvtdata.station_data_clean,date1,date2,display=True)
 #print(dvtcalc.kpi)
 #dvtcalc.key_charts()
 #print(dvtcalc.maxdate)
